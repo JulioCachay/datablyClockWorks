83d4fe5eaccb18172ad61481abb54204e4671b54
Julio <julio.cachay@datably.io>
2023-07-21 16:27:19 -0400

    Added ReportComposite.cs

---
+++ DatablyPerformanceReporter.UnitTests/ReportCompositeTests.cs
@@ -0,0 +1,138 @@
+﻿namespace DatablyPerformanceReporter.UnitTests;
+
+public class ReportCompositeTests
+{
+    private ReportComposite CreateReportCompositeLeaf(
+        string description, int ellapsedMs)
+    {
+        var ellapsed = TimeSpan.FromMilliseconds(ellapsedMs);
+
+        return new(null, description, ellapsed);
+    }
+
+    private ReportComposite CreateReportCompositeLeaf(int ellapsedMs)
+    {
+        var ellapsed = TimeSpan.FromMilliseconds(ellapsedMs);
+
+        return new(null, Guid.NewGuid().ToString(), ellapsed);
+    }
+
+    [Fact]
+    public void Constructor_SetRootData()
+    {
+        // ************ ARRANGE ************
+        
+        // ************ ACT ************
+
+        var sut = CreateReportCompositeLeaf("some request", 500);
+        
+        // ************ ASSERT ************
+        
+        Assert.Equal("some request", sut.Description);
+        Assert.Equal(500, sut.Elapsed.TotalMilliseconds);
+    }
+    
+    [Fact]
+    public void CanAddChild_SettingChildParent()
+    {
+        // ************ ARRANGE ************
+
+        var parent = CreateReportCompositeLeaf("some request", 100);
+
+        var child = CreateReportCompositeLeaf("another request", 500);
+        
+        // ************ ACT ************
+
+        parent.AddChild(child);
+        
+        // ************ ASSERT ************
+
+        Assert.Contains(parent.Children, c =>c.Equals(child));
+        Assert.Equal(parent, child.Parent);
+    }
+
+
+    [Fact]
+    public void TimeSlicePercentage_NoParent_Returns100Percent()
+    {
+        // ************ ARRANGE ************
+
+        var sut = CreateReportCompositeLeaf("some request", 500);
+
+        // ************ ACT ************
+
+        var result = sut.TimeSlicePercentage;
+
+        // ************ ASSERT ************
+        
+        Assert.Equal(100M, result);
+    }
+
+    [Fact]
+    public void TimeSlicePercentage_HasParent_ReturnsEllapsedPercengateRespectToParentEllapsed()
+    {
+        // ************ ARRANGE ************
+
+        var parent = CreateReportCompositeLeaf("some request", 1000);
+
+        var child = CreateReportCompositeLeaf("another request", 100);
+        
+        parent.AddChild(child);
+
+        // ************ ACT ************
+
+        var result = child.TimeSlicePercentage;
+
+        // ************ ASSERT ************
+        
+        Assert.Equal(10M, result);
+    }
+
+    [Fact]
+    public void GenerateReport_NoChildren_ReportsItself()
+    {
+        // ************ ARRANGE ************
+
+        var sut = CreateReportCompositeLeaf(1000);
+
+        // ************ ACT ************
+
+        var result = sut.GenerateReport();
+
+        // ************ ASSERT ************
+        
+        VerifyLeafDataInString(result, sut);
+        
+    }
+
+    [Fact]
+    public void GenerateReport_ReportsItself_Plus_EachChild()
+    {
+        // ************ ARRANGE ************
+        
+        var parent = CreateReportCompositeLeaf(1000);
+        var child1 = CreateReportCompositeLeaf(400);
+        var child2 = CreateReportCompositeLeaf(400);
+        
+        parent.AddChild(child1);
+        parent.AddChild(child2);
+
+        // ************ ACT ************
+        
+        var result = parent.GenerateReport();
+
+        // ************ ASSERT ************
+        
+        VerifyLeafDataInString(result, child1);
+        VerifyLeafDataInString(result, child2);
+    }
+
+    private void VerifyLeafDataInString(string searchString, ReportComposite leaf)
+    {
+        Assert.True(searchString.IndexOf(leaf.Description)>=0);
+        var elapsed = ((decimal)leaf.Elapsed.TotalMilliseconds).ToString();
+        Assert.True(searchString.IndexOf(elapsed)>=0);
+        Assert.True(searchString.IndexOf(leaf.TimeSlicePercentage.ToString())>=0);
+    }
+
+}
---
+++ DatablyPerformanceReporter/ReportComposite.cs
@@ -0,0 +1,71 @@
+﻿using System.Timers;
+
+namespace DatablyPerformanceReporter;
+
+public class ReportComposite
+{
+    public ReportComposite(ReportComposite? parent, string description, TimeSpan elapsed)
+    {
+        Parent = parent;
+        Description = description;
+        Elapsed = elapsed;
+    }
+
+    public void AddChild(ReportComposite child)
+    {
+        child.Parent = this;
+        Children = Children.Append(child).ToArray();
+    }
+    public ReportComposite? Parent { get; set; }
+    public string Description { get; }
+    public TimeSpan Elapsed { get; }
+
+    public decimal TimeSlicePercentage => CalculateTimeSlicePercentage();
+
+    private decimal CalculateTimeSlicePercentage()
+    {
+        if (Parent is null)
+        {
+            return 100M;
+        }
+
+        if (Parent.Elapsed.TotalMilliseconds <= 0)
+        {
+            return 0;
+        }
+        return (decimal)(Elapsed.TotalMilliseconds / Parent.Elapsed.TotalMilliseconds * 100);
+    }
+
+    public IEnumerable<ReportComposite> Children { get; private set; } 
+        = Array.Empty<ReportComposite>();
+
+    public string GenerateReport()
+    {
+        var result = Environment.NewLine + FormatRow(Description, Elapsed, TimeSlicePercentage);
+        foreach (var c in Children)
+        {
+            result += Environment.NewLine+ "-" + c.GenerateReport();
+        }
+        /*
+         * --------------------------------------------------
+         * - Request 1                1000 ms.    23 %   
+         * - Request 2                1850  ms.   15 %   
+         * -- Task 1                   850  ms.   15 %    
+         * -- Task 2                   850  ms.   15 %  
+         * __________________________________________________
+         * Request 3                  900 ms.     13 %
+         * --------------------------------------------------
+         */
+        return result;
+    }
+
+
+    private string FormatRow(
+        string description, TimeSpan elapsed, decimal percentage)
+    {
+        var d = description.PadRight(90);
+        var e = (elapsed.TotalMilliseconds.ToString() + " ms").PadRight(10);
+        var p = (percentage.ToString() + " %").PadRight(10);
+        return d + "| " + e + "| " + p;
+    }
+}
